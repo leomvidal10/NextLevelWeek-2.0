43129e290340ecc5682b4dd5d97f46c18d3ba9aa
Leonardo Martins <leomvidal10@gmail.com>
2020-10-03 13:14:30 -0300

    Projeto desenvolvido durante a next level week.

---
+++ mobile/src/components/PageHeader/styles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 40,
+        backgroundColor: '#8257e5'
+    },
+
+    topBar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+
+    iconBack: {
+        padding: 10
+    },
+
+    title: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#fff',
+        fontSize: 24,
+        lineHeight: 32,
+        maxWidth: 160,
+        marginVertical: 40,
+    }
+})
+
+export default styles;
